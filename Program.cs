b272851d9bb8ae6b8c6ac3aaea554e9c70ad95ee
Gobba <Gobba@GOBBA>
2022-02-11 14:14:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] teams = { "Бавария", "Боруссия", "Реал Мадрид", "Манчестер Сити", "ПСЖ", "Барселона" };
+
+            var selectedTeams = new List<string>();
+            foreach (string s in teams)
+            {
+                if (s.ToUpper().StartsWith("Б"))
+                    selectedTeams.Add(s);
+            }
+            selectedTeams.Sort();
+
+            foreach (string s in selectedTeams)
+                Console.WriteLine(s);
+        }
+    }
+}
